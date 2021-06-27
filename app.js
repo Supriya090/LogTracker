eb417e54bb8ed64837f4abf45d59cd3ea801acb3
rahulshah63 <075bct063.rahul@pcampus.edu.np>
2021-06-27 12:24:28 +0545

    Added Sign up and Login feature using Mongo

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var dotenv = require('dotenv');
+var connectDB = require('./config/mongo');
+var session = require('express-session');
+var passport = require('passport');
 
+//Passport configs
+require('./passport')(passport)
+
+//Load configs
+dotenv.config({ path: './config/config.env' });
+connectDB()
+
+//Routes
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth")(passport);
 
 var app = express();
 
@@ -20,14 +33,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//Sessions
+app.use(session({
+  secret: process.env.Secret,
+  saveUninitialized: false,
+  resave: false
+}));
+
+//Passport middlewares
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Body parser
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
+//@Routers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
+//to verify login and protecting from login bypass
+var loggedin = function(req, res, next){
+  if(req.isAuthenticated()){
+    next()
+  }
+  else{
+    res.redirect("/")
+  }
+}
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Log Tracker | Login" });
+});
+
+/* GET signup page. */
+router.get("/signup", function (req, res, next) {
+  res.render("signup", { title: "Log Tracker | Sign Up" });
+});
+
+/* GET Dashboard. */
+router.get("/dashboard", loggedin, function (req, res, next) {
+  res.send(req.session);
+});
+
+/* Logout Session. */
+router.get("/logout",function(req, res, next){
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;

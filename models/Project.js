01f3e6823fb2e5d4cbc03b5dc9e90267a02c9824
Ranju <ranjugc69@gmail.com>
2021-07-04 09:26:37 +0545

    project functions

--- models/Project.js
+++ models/Project.js
@@ -1,116 +1,129 @@
-// var mongoose = require('mongoose')
+var mongoose = require('mongoose')
 
 
-// //Models
-// var ProjectSchema = new mongoose.Schema({
-//     createdDate: {
-//         type: Date,
-//         default: Date.now(),
-//         required: true,
-//     },
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     semester: {
-//         type: String,
-//         required: true
-//     },
-//     members:{
-//         supervisor:String,
-//         team:[String]
-//     },
-//     events: [{
-//         title: String,
-//         status: String,
-//     }],
-//     createdBy: {
-//         type: String,
-//         required: true
-//     },
-//     isCompleted: {
-//         type: Boolean,
-//         default: false,
-//         required: true
-//     }
-// })
+//Models
+var ProjectSchema = new mongoose.Schema({
+    createdDate: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: String,
+        required: true
+    },
+    members:{
+        supervisor:String,
+        team:[String]
+    },
+    events: [{
+        title: String,
+        status: String,
+    }],
+    createdBy: {
+        type: String,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+        required: true
+    }
+})
 
 
-// var Project = module.exports = mongoose.model('Project', ProjectSchema, 'projects')
+var Project = module.exports = mongoose.model('Project', ProjectSchema, 'projects')
+
+module.exports.createProject = function (newProject, callback) {
+    newProject.save(callback)
+}
+
+module.exports.getProjectsbyUser = function (userId, callback) {
+    let query = {
+        projectId: pid
+    }
+    Minute.find(query, callback)
+}
+
+module.exports.getProjectsbyId = function (projectId, callback) {
+    let query = {
+        _id: projectId
+    }
+    Minute.find(query, callback)
+}
+
+module.exports.getProjectsbySemester = function (sem, callback) {
+    let query = {
+        semester: sem
+    }
+    Minute.find(query, callback)
+}
 
 
-// module.exports.getMinutesbyPid = function (pid, callback) {
-//     let query = {
-//         projectId: pid
-//     }
-//     Minute.find(query, callback)
-// }
+module.exports.updateProjectbyId = function (projectId, newProject, callback) {
+    let query = {
+        _id: projectId
+    }
 
-// module.exports.createMinute = function (newMinute, callback) {
-//     newMinute.save(callback)
-// }
-// module.exports.updateMinutebyId = function (pid, minuteId, newMinute, callback) {
-//     let query = {
-//         projectname: pid
-//     }
+    Project.find(query, function (err, m) {
+        if (err) throw err
 
-//     Minute.find(query, function (err, m) {
-//         if (err) throw err
+        //minutes exist in database
 
-//         //minutes exist in database
+        if (m.length > 0) {
+            Minute.findOneAndUpdate({
+                    projectname: pid,
+                }, {
+                    $set: {
+                        projectId: pid,
+                        title: newMinute.title,
+                        description: newMinute.description,
+                        updatedBy: todo
+                    }
+                }, {
+                    new: true
+                },
+                callback
+            )
+        }
+    })
+}
 
-//         if (m.length > 0) {
-//             Minute.findOneAndUpdate({
-//                     projectname: pid,
-//                 }, {
-//                     $set: {
-//                         projectId: pid,
-//                         title: newMinute.title,
-//                         description: newMinute.description,
-//                         updatedBy: todo
-//                     }
-//                 }, {
-//                     new: true
-//                 },
-//                 callback
-//             )
-//         } else {
-//             newMinute.save(callback)
-//         }
-//     })
-// }
+module.exports.verifyMinute = function (pid, minuteId, newMinute, callback) {
+    let query = {
+        projectname: pid
+    }
 
-// module.exports.verifyMinute = function (pid, minuteId, newMinute, callback) {
-//     let query = {
-//         projectname: pid
-//     }
+    Minute.find(query, function (err, m) {
+        if (err) throw err
 
-//     Minute.find(query, function (err, m) {
-//         if (err) throw err
+        //minutes exist in database
 
-//         //minutes exist in database
-
-//         if (m.length > 0) {
-//             Minute.findOneAndUpdate({
-//                     projectname: pid,
-//                 }, {
-//                     $set: {
-//                         projectId: pid,
-//                         title: newMinute.title,
-//                         description: newMinute.description,
-//                         updatedBy: todo
-//                     }
-//                 }, {
-//                     new: true
-//                 },
-//                 callback
-//             )
-//         } else {
-//             newMinute.save(callback)
-//         }
-//     })
-// }
+        if (m.length > 0) {
+            Minute.findOneAndUpdate({
+                    projectname: pid,
+                }, {
+                    $set: {
+                        projectId: pid,
+                        title: newMinute.title,
+                        description: newMinute.description,
+                        updatedBy: todo
+                    }
+                }, {
+                    new: true
+                },
+                callback
+            )
+        } else {
+            newMinute.save(callback)
+        }
+    })
+}

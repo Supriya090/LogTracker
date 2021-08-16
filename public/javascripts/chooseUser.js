8cf07bda4ca32b269ebcc247b0791617e61a6c21
Supriya090 <khadkasupriya76@gmail.com>
2021-08-16 11:42:07 +0545

    edit team error fixed

--- public/javascripts/chooseUser.js
+++ public/javascripts/chooseUser.js
@@ -1,15 +1,20 @@
 $(document).ready(function (e) {
   $(".addSuper").on("click", addSuper);
+  const superPastChildren = $("#new_supervisor").children().length;
+  const studPastChildren = $("#new_student").children().length;
+  console.log(superPastChildren);
 
   function addSuper() {
     var lastField = $("#new_supervisor div:last");
+    console.log(lastField.data("idx"))
     var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
+    console.log(intId);
     var userSelect = document.getElementById("addSuper");
     var fieldWrapper = $(
-      '<div class="fieldwrapper" id="field' + intId + '"/>'
+      '<div class="fieldwrapper" id="field' + (intId + superPastChildren) + '"/>'
     );
     fieldWrapper.data("idx", intId);
-    var new_input = $("<input class='button-type' name='supervisor" + intId + "' value ='" + userSelect.value + "' readonly />")
+    var new_input = $("<input class='button-type' name='supervisor" + (intId + superPastChildren) + "' value ='" + userSelect.value + "' readonly />")
     var removeButton = $(
       '<input type="button" class="remove" value="-" />'
     );
@@ -31,10 +36,10 @@ $(document).ready(function (e) {
     var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
     var userSelect = document.getElementById("addStudent");
     var fieldWrapper = $(
-      '<div class="fieldwrapper" id="studField' + intId + '"/>'
+      '<div class="fieldwrapper" id="studField' + (intId + studPastChildren) + '"/>'
     );
     fieldWrapper.data("idx", intId);
-    var new_input = $("<input class='button-type' name='std" + intId + "' value ='" + userSelect.value + "' readonly />")
+    var new_input = $("<input class='button-type' name='std" + (intId + studPastChildren) + "' value ='" + userSelect.value + "' readonly />")
     var removeButton = $(
       '<input type="button" class="remove" value="-" />'
     );
